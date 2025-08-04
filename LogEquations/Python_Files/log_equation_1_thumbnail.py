8c0d1031f089653df0d3f992319fdc1044754a0a
McBrown Mwale <mcbrownwilfred@gmail.com>
2025-08-04 12:57:22 +0200

    Update project

--- LogEquations/Python_Files/log_equation_1_thumbnail.py
+++ LogEquations/Python_Files/log_equation_1_thumbnail.py
@@ -11,16 +11,16 @@ class Thumbnail(Scene):
         
         # Title text
         title = Text(
-            "Rationalizing", font="Roboto", weight=BOLD, color=YELLOW
+            "Solving", font="Roboto", weight=BOLD, color=YELLOW
         ).scale(1.5).to_edge(UP)
 
         subtitle = Text(
-            "the Denominator", font="Roboto", weight=BOLD, color=WHITE
+            "Logarithmic Equation", font="Roboto", weight=BOLD, color=WHITE
         ).scale(1.5).next_to(title, DOWN, buff=0.3)
 
         # Quadratic Formula
         formula = MathTex(
-            r"x = \frac{6}{\sqrt{32}}", color=WHITE
+            r"2\log y = \log(3y + 4)", color=WHITE
         ).scale(1.7).next_to(subtitle, DOWN, buff=1)
 
         # Add everything
