b148005bf1f3101bb9331153bf5a3eb94bb6701c
McBrown Mwale <mcbrownwilfred@gmail.com>
2025-08-15 09:45:53 +0200

    Modify range_domain_1_thumbnail.py

--- Functions/range_domain_1/range_domain_1_thumbnail.py
+++ Functions/range_domain_1/range_domain_1_thumbnail.py
@@ -3,7 +3,7 @@ from manim import *
 class Thumbnail(Scene):
     def construct(self):
         # Add background image
-        background = ImageMobject("../Image/chalk_board.jpg")
+        background = ImageMobject("./Image/chalk_board.jpg")
         background.set_z_index(-1)
         background.scale_to_fit_height(config.frame_height)
         background.scale_to_fit_width(config.frame_width)
