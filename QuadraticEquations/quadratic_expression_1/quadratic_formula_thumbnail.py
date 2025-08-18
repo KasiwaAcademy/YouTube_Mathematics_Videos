b1bf0c2897d0170ec01fa2692c8a3521dc418bae
McBrown Mwale <mcbrownwilfred@gmail.com>
2025-08-18 16:12:48 +0200

    Initialise a new project quadratic_expression_1.ipynb and add a thumbnail for the project

---
+++ QuadraticEquations/quadratic_expression_1/quadratic_formula_thumbnail.py
@@ -0,0 +1,27 @@
+from manim import *
+
+class Thumbnail(Scene):
+    def construct(self):
+        # Add background image
+        background = ImageMobject("../Image/chalk_board.jpg")
+        background.set_z_index(-1)
+        background.scale_to_fit_height(config.frame_height)
+        background.scale_to_fit_width(config.frame_width)
+        self.add(background)
+        
+        # Title text
+        title = Text(
+            "Factorizing", font="Roboto", weight=BOLD, color=YELLOW
+        ).scale(1.5).to_edge(UP)
+
+        subtitle = Text(
+            "a Quadratic Expression", font="Roboto", weight=BOLD, color=WHITE
+        ).scale(1.5).next_to(title, DOWN, buff=0.3)
+
+        # Quadratic Formula
+        formula = MathTex(
+            r"10 + 8m - 24m^2", color=WHITE
+        ).scale(1.7).next_to(subtitle, DOWN, buff=1)
+
+        # Add everything
+        self.add(title, subtitle, formula)
